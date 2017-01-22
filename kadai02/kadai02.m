f556897dfe11c76c47efb5b4979921127f37d3e2
ogata3 <14ec028@ms.dendai.ac.jp>
2017-01-22 22:36:33 +0900

    Add files via upload

---
+++ kadai02/kadai02.m
@@ -0,0 +1,30 @@
+clear; % •Ï”‚ÌƒI[ƒ‹ƒNƒŠƒA
+
+ORG=imread('kadai02.jpg'); % Œ´‰æ‘œ‚Ì“ü—Í
+ORG = rgb2gray(ORG); colormap(gray); colorbar;
+imagesc(ORG); axis image; % ‰æ‘œ‚Ì•\Ž¦
+pause; % ˆêŽž’âŽ~
+
+% ‚QŠK’²‰æ‘œ‚Ì¶¬
+IMG = ORG>128;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
+
+% ‚SŠK’²‰æ‘œ‚Ì¶¬
+IMG0 = ORG>64;
+IMG1 = ORG>128;
+IMG2 = ORG>192;
+IMG = IMG0 + IMG1 + IMG2;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
+
+% ‚WŠK’²‰æ‘œ‚Ì¶¬
+IMG0 = ORG>32;
+IMG1 = ORG>64;
+IMG2 = ORG>96;
+IMG3 = ORG>128;
+IMG4 = ORG>160;
+IMG5 = ORG>192;
+IMG6 = ORG>224;
+IMG = IMG0 + IMG1 + IMG2 + IMG3 + IMG4 + IMG5 + IMG6;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
