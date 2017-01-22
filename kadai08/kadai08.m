e133a74589e543c0b4e4f21d8c6eac693a43a85d
ogata3 <14ec028@ms.dendai.ac.jp>
2017-01-23 00:47:59 +0900

    Add files via upload

---
+++ kadai08/kadai08.m
@@ -0,0 +1,11 @@
+clear;
+
+ORG = imread('kadai08.png'); % 画像の読み込み
+ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+IMG = ORG > 128; % 閾値128で二値化
+imagesc(IMG); colormap(gray); colorbar; % 画像の表示
+pause;
+IMG = bwlabeln(IMG);
+imagesc(IMG); colormap(jet); colorbar; % 画像の表示
