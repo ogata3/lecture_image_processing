2b7ab21ce0e3ac7b2c747ec3cfbe32873bc6e202
ogata3 <14ec028@ms.dendai.ac.jp>
2017-01-23 00:40:25 +0900

    Add files via upload

---
+++ kadai07/kadai07.m
@@ -0,0 +1,16 @@
+clear;
+
+ORG = imread('kadai07.png'); % 画像の読み込み
+ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+imhist(ORG); % 濃度ヒストグラムを生成、表示
+pause;
+ORG = double(ORG);
+mn = min(ORG(:)); % 濃度値の最小値を算出
+mx = max(ORG(:)); % 濃度値の最大値を算出
+ORG = (ORG-mn)/(mx-mn)*255;
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+ORG = uint8(ORG); % この行について考察せよ
+imhist(ORG); % 濃度ヒストグラムを生成、表示
